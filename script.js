e7dfd61368c7e70384dc17f1f3c519e138f7c989
amruthakalyan <116357692+amruthakalyan@users.noreply.github.com>
2023-08-13 13:07:50 +0530

    randomNumberGenerator

---
+++ script.js
@@ -0,0 +1,26 @@
+
+    let img1 = document.getElementById("win-emoji");
+    let img2 = document.getElementById("loss-emoji");
+ let num= prompt("Enter the no from 1 to 10 only");
+ let randomNum = Math.floor(Math.random()*10);
+ console.log(randomNum);
+ if(num >10){
+    alert("Please enter the number between 1 to 10 only");
+ }
+ else{
+ if(num == randomNum){
+ alert("YOU WON\n The number matched");
+ }
+ else{
+    alert("YOU LOSS\n The number not matched\n The no. was "+ randomNum);
+ }
+}
+ function emoji(){
+    if(num == randomNum){
+        img1.style.display='block';
+    }
+    else{
+        img2.style.display='block';
+    }
+ }
+ emoji();
