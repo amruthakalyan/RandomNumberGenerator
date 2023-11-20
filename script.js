cd3e0a0170d66268faaa0226a7cbcb73992ee111
amruthakalyan <116357692+amruthakalyan@users.noreply.github.com>
2023-11-20 22:14:25 +0530

    Update script.js

--- script.js
+++ script.js
@@ -4,11 +4,13 @@ let loss_sound = new Audio('archivo.mp3');
     let img2 = document.getElementById("loss-emoji");
     let btn = document.getElementById("btn");
 
- let num= prompt("Enter the no from 1 to 5 only");
- let randomNum = Math.floor(Math.random()*5);
+
+ function game(){
+  let num= prompt("Enter the no from 0 to 3 only");
+ let randomNum = Math.floor(Math.random()*3);
  console.log(randomNum);
 if(num >5){
-    alert("Please enter the number between 1 to 5 only");
+    alert("Please enter the number between 0 to 3 only");
  }
  else if(num.length==0 ||num ===' '|| num==null){
    alert("Please Enter the number");
@@ -22,22 +24,22 @@ if(num >5){
  else{
     alert("YOU LOSS\n The no. was "+ randomNum);
  }
-
-
-
- function emoji(){
-    if(num == randomNum){
-        img1.style.display='block';
-        btn.style.display = 'block';
-        win_sound.play();
-    }
-    else{
-        img2.style.display='block';
-        btn.style.display = 'block';
-        loss_sound.play();
-    }
+ if(num == randomNum){
+  img1.style.display='block';
+  img2.style.display='none';
+  btn.style.display = 'block';
+  win_sound.play();
+}
+else{
+  img2.style.display='block';
+  img1.style.display='none';
+  btn.style.display = 'block';
+  loss_sound.play();
+}
  }
- emoji();
+game();
+
+ 
  btn.addEventListener("click",()=>{
-   document.location.reload();
+     game();
  })
